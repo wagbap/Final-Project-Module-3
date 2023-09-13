b35c1f1784646bf003b0c811211e0faa9355d43d
Wagner Baptista <wagner.h2@hotmail.com>
2023-09-13 12:52:31 +0100

    First Commit of project organize the folders

---
+++ LibBiz/Biblio/Biblio/API.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio
+{
+    public class API
+    {
+
+        public class Result
+        {
+            public int retCode { get; set; }
+            public string retError { get; set; }
+            public string retActionDateTime { get; set; }
+        }
+        public class Result_Ext : Result
+        {
+            public DataSet ds { get; set; }
+        }
+        public class Prm_Login
+        {
+            public string User { get; set; }
+            public string Password { get; set; }
+        }
+        public class Prm_SQL
+        {
+            public string[] SQL { get; set; }
+        }
+
+        public static void Login(Prm_Login prmIn, out Result_Ext prmOut, SqlConnection cn = null, string cn_string = null)
+        {
+            prmOut = new Result_Ext();
+            prmOut.ds = new DataSet("Tables");
+
+            SqlConnection connection = null;
+            bool bCloseConnection = false;
+
+            try
+            {
+                connection = cn;
+                if (connection == null)
+                {
+                    connection = new SqlConnection(cn_string);
+                    bCloseConnection = true;
+                    connection.Open();
+                }
+
+                using (SqlCommand cmd = new SqlCommand("SistemaLogin", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@User", prmIn.User);
+                    cmd.Parameters.AddWithValue("@Password", prmIn.Password);
+                    using (DataSet ds = new DataSet())
+                    {
+                        using (var da = new SqlDataAdapter(cmd))
+                        {
+                            da.Fill(prmOut.ds);
+
+                            if (prmOut.ds.Tables[0].Rows.Count != 1)
+                            {
+                                throw new Exception("Username/Password not valid.");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+            finally
+            {
+                if (bCloseConnection && connection != null) { connection.Close(); }
+            }
+        }
+
+        public static void Execute_SQL(Prm_SQL prmIn, out Result_Ext prmOut, SqlConnection cn = null, string cn_string = null)
+        {
+            prmOut = new Result_Ext();
+            prmOut.ds = new DataSet();
+
+            SqlConnection connection = null;
+            bool bCloseConnection = false;
+
+            try
+            {
+                connection = cn;
+                if (connection == null)
+                {
+                    connection = new SqlConnection(cn_string);
+                    bCloseConnection = true;
+                    connection.Open();
+                }
+
+                foreach (string sql in prmIn.SQL)
+                {
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        using (DataTable dt = new DataTable())
+                        {
+                            using (var da = new SqlDataAdapter(cmd)) { da.Fill(dt); prmOut.ds.Tables.Add(dt); }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+            finally
+            {
+                if (bCloseConnection && connection != null) { connection.Close(); }
+            }
+        }
+    }
+}
---
+++ LibBiz/Biblio/Biblio/Gateway.cs
@@ -0,0 +1,82 @@
+﻿using Cursos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Utilis;
+using static Biblio.API;
+
+namespace Biblio
+{
+    public class Gateway
+    {
+
+        SqlConnection cn = null;
+        DataSet ds = null;
+        public static string ExecuteLogin(Prm_Login prmIn)
+        {
+
+            string currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string filePath = Path.Combine(currentDirectory, "ConsoleADONET1.ini");
+            Utils.INIFile ini = new Utils.INIFile(filePath);
+
+
+            string cnString = ini.ReadKey("Connection_Biblio");
+
+            int iRC = 0;
+            string sMsgErr = "";
+
+            Result_Ext prmOutExt = null;
+            Result prmOut = null;
+
+            try
+            {
+                Login(prmIn, out prmOutExt, null, cnString);
+                prmOut = prmOutExt;
+
+                iRC = 1;
+            }
+            catch (Exception ex)
+            {
+                iRC = 0;
+                sMsgErr = ex.Message;
+            }
+            finally
+            {
+            }
+
+            return Utilities.ActionJsonResponse(iRC, sMsgErr, prmOut);
+        }
+
+        public static string ExecuteSQL(Prm_SQL prmIn, string cn_string = null)
+        {
+            int iRC = 0;
+            string sMsgErr = "";
+
+            Result_Ext prmOutExt = null;
+            Result prmOut = null;
+
+            try
+            {
+                Execute_SQL(prmIn, out prmOutExt, null, cn_string);
+                prmOut = prmOutExt;
+
+                iRC = 1;
+            }
+            catch (Exception ex)
+            {
+                iRC = 0;
+                sMsgErr = ex.Message;
+            }
+            finally
+            {
+            }
+
+            return Utilities.ActionJsonResponse(iRC, sMsgErr, prmOut);
+        }
+    }
+}
